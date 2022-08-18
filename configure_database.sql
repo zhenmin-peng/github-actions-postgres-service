dcdc0e08ebd55de7490326dbdd71149df077b9b4
Zhenmin Peng <zhenmin@bondorigination.com>
2022-08-18 14:25:27 +0100

    update

--- configure_database.sql
+++ configure_database.sql
@@ -12,13 +12,14 @@ BEGIN
    ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT ALL ON TABLES TO rw_only;
 END
 $do$;
+
 DO
 $do$
 BEGIN
-   IF EXISTS (SELECT FROM pg_catalog.pg_roles WHERE  rolname = :db_username) THEN
-      RAISE NOTICE 'User % already exists! Skipping.', :db_username;
+   IF EXISTS (SELECT FROM pg_catalog.pg_roles WHERE  rolname = ':db_username') THEN
+      RAISE NOTICE 'User % already exists! Skipping.', ':db_username';
    ELSE
-      CREATE USER :db_username WITH PASSWORD :db_password;
+      CREATE USER :db_username WITH PASSWORD ':db_password';
    END IF;
    GRANT rw_only TO :db_username;
 END
